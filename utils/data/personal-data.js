46b59780b0267992ef38c53a0822130fbc8210b5
mkapoor-datopic <mkapoor@datopic.com>
2025-03-18 16:17:46 +0530

    resume changed

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -8,5 +8,5 @@ export const personalData = {
   address: 'Malla gorakhpur vikaspuri lane-2 , Haldwani ',
   github: 'https://github.com/ManasKapoor07',
   linkedIn: 'https://www.linkedin.com/in/manas-kapoor-51b5a7215/',
-  resume: "https://resume-manas.s3.eu-north-1.amazonaws.com/Manas-resume.pdf"
+  resume: "https://resume-manas.s3.eu-north-1.amazonaws.com/Manas_Kapoor.pdf"
 }
